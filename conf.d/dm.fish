aec4a7fb98af3a2402f3aa20e853f6a5f7feeb53
Alain Armand <aarmand.inbox@gmail.com>
2020-12-11 21:03:54 +0100

    Update dm.fish

--- conf.d/dm.fish
+++ conf.d/dm.fish
@@ -1,7 +1,5 @@
 
-# function dotfiles --wraps '/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME' --description 'alias dotfiles=/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-#   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv;
-# end
+
 alias dm='git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
 
 
